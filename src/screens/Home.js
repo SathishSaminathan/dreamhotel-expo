cc6aab8600cd8840f1f3aa5cdcd6bd1e71ad87dd
SathishSaminathan <Sathishcse1975@gmail.com>
2020-05-01 20:08:32 +0530

    commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   Text,
   View,
@@ -6,22 +6,22 @@ import {
   StatusBar,
   ImageBackground,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
 
-import TextComponent from '../components/Shared/TextComponent';
-import {Colors} from '../constants/ThemeConstants';
-import ImageComponent from '../components/Shared/ImageComponent';
-import {Images} from '../assets/images';
-import {widthPerc, heightPerc} from '../helpers/styleHelper';
-import IconComponent from '../components/Shared/IconComponent';
-import {IconType, FontType} from '../constants/AppConstants';
-import {TouchableNativeFeedback} from 'react-native-gesture-handler';
-import PoweredBY from '../components/Shared/PoweredBy';
+import TextComponent from "../components/Shared/TextComponent";
+import { Colors } from "../constants/ThemeConstants";
+import ImageComponent from "../components/Shared/ImageComponent";
+import { Images } from "../assets/images";
+import { widthPerc, heightPerc } from "../helpers/styleHelper";
+import IconComponent from "../components/Shared/IconComponent";
+import { IconType, FontType } from "../constants/AppConstants";
+import { TouchableNativeFeedback } from "react-native-gesture-handler";
+import PoweredBY from "../components/Shared/PoweredBy";
 
 export default class Home extends Component {
   componentDidMount() {
-    this.navListener = this.props.navigation.addListener('didFocus', () => {
-      StatusBar.setBarStyle('light-content');
+    this.navListener = this.props.navigation.addListener("didFocus", () => {
+      StatusBar.setBarStyle("light-content");
       StatusBar.setBackgroundColor(Colors.primaryThemeColor);
     });
   }
@@ -30,7 +30,7 @@ export default class Home extends Component {
       <View style={styles.container}>
         <StatusBar
           backgroundColor={Colors.white}
-          barStyle={'dark-content'}
+          barStyle={"dark-content"}
           hidden={false}
           translucent={false}
         />
@@ -39,64 +39,75 @@ export default class Home extends Component {
             height: 80,
             backgroundColor: Colors.white,
             width: widthPerc(100),
-            justifyContent: 'center',
-            alignItems: 'center',
+            justifyContent: "center",
+            alignItems: "center",
             elevation: 10,
-          }}>
+          }}
+        >
           <View
             style={{
               width: widthPerc(60),
               height: 60,
-            }}>
+            }}
+          >
             <ImageComponent
               source={Images.LogoDark}
-              style={{width: undefined, height: undefined, flex: 1}}
+              style={{ width: undefined, height: undefined, flex: 1 }}
               resizeMode="contain"
             />
           </View>
-          <View style={{position: 'absolute', right: '5%'}}>
+          <View style={{ position: "absolute", right: "5%" }}>
             <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('Drawer')}
-              style={{padding: 10}}>
+              activeOpacity={0.8}
+              onPress={() => this.props.navigation.navigate("Drawer")}
+              style={{ padding: 10 }}
+            >
               <IconComponent
                 type={IconType.Ionicons}
                 name="ios-menu"
-                style={{fontSize: 30}}
+                style={{ fontSize: 30 }}
               />
             </TouchableOpacity>
           </View>
         </View>
         <ImageBackground source={Images.group15} style={styles.image}>
-          <View style={{flex: 1}}>
+          <View style={{ flex: 1 }}>
             <View
               style={{
-                flexDirection: 'row',
-                width: '100%',
-                justifyContent: 'space-between',
-              }}>
+                flexDirection: "row",
+                width: "100%",
+                justifyContent: "space-between",
+              }}
+            >
               <View>
                 <TextComponent
-                  style={{fontSize: 16, color: Colors.white}}
-                  type={FontType.BOLD}>
+                  style={{ fontSize: 16, color: Colors.white }}
+                  type={FontType.BOLD}
+                >
                   Hi, John
                 </TextComponent>
-                <TextComponent style={{fontSize: 10, color: Colors.white}}>
+                <TextComponent style={{ fontSize: 10, color: Colors.white }}>
                   Alfred Loyalty Points 1500
                 </TextComponent>
               </View>
-              <View style={{flexDirection: 'row'}}>
+              <View style={{ flexDirection: "row" }}>
                 <TextComponent
-                  style={{fontSize: 32, paddingRight: 10, color: Colors.white}}>
+                  style={{
+                    fontSize: 32,
+                    paddingRight: 10,
+                    color: Colors.white,
+                  }}
+                >
                   73°F
                 </TextComponent>
                 <View>
-                  <TextComponent style={{fontSize: 10, color: Colors.white}}>
+                  <TextComponent style={{ fontSize: 10, color: Colors.white }}>
                     Partly cloudy
                   </TextComponent>
-                  <TextComponent style={{fontSize: 10, color: Colors.white}}>
+                  <TextComponent style={{ fontSize: 10, color: Colors.white }}>
                     Sat, Mar 21
                   </TextComponent>
-                  <TextComponent style={{fontSize: 10, color: Colors.white}}>
+                  <TextComponent style={{ fontSize: 10, color: Colors.white }}>
                     Nashville
                   </TextComponent>
                 </View>
@@ -104,23 +115,27 @@ export default class Home extends Component {
             </View>
             <TouchableOpacity
               activeOpacity={1}
-              onPress={() => this.props.navigation.navigate('Location')}>
+              onPress={() => this.props.navigation.navigate("Location")}
+            >
               <View
                 style={{
                   backgroundColor: Colors.white,
                   borderRadius: 50,
                   paddingVertical: 15,
                   paddingHorizontal: 20,
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
                   marginTop: 20,
-                }}>
-                <TextComponent style={{fontSize: 13, color: Colors.searchText}}>
+                }}
+              >
+                <TextComponent
+                  style={{ fontSize: 13, color: Colors.searchText }}
+                >
                   Search your Location
                 </TextComponent>
                 <IconComponent
-                  style={{fontSize: 15, color: Colors.searchText}}
+                  style={{ fontSize: 15, color: Colors.searchText }}
                   type={IconType.Feather}
                   name="search"
                 />
@@ -128,7 +143,7 @@ export default class Home extends Component {
             </TouchableOpacity>
           </View>
 
-          <View style={{paddingVertical: 10}}>
+          <View style={{ paddingVertical: 10 }}>
             <PoweredBY />
           </View>
         </ImageBackground>
@@ -140,12 +155,12 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   image: {
     flex: 1,
-    resizeMode: 'contain',
-    justifyContent: 'space-between',
+    resizeMode: "contain",
+    justifyContent: "space-between",
     paddingHorizontal: 30,
     paddingTop: 20,
   },
