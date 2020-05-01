d10ca6ac5ff00a8523fc33c615f69b28714f7ad9
SathishSaminathan <Sathishcse1975@gmail.com>
2020-05-01 20:10:00 +0530

    commit for account

--- src/screens/Account/Account.js
+++ src/screens/Account/Account.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
-import {Text, View, ScrollView, TouchableNativeFeedback} from 'react-native';
-import TextComponent from '../../components/Shared/TextComponent';
-import {FontType, IconType} from '../../constants/AppConstants';
-import {Colors} from '../../constants/ThemeConstants';
-import IconComponent from '../../components/Shared/IconComponent';
-import ButtonComponent from '../../components/Shared/ButtonComponent';
-import PoweredBY from '../../components/Shared/PoweredBy';
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  ScrollView,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+} from "react-native";
+import TextComponent from "../../components/Shared/TextComponent";
+import { FontType, IconType } from "../../constants/AppConstants";
+import { Colors } from "../../constants/ThemeConstants";
+import IconComponent from "../../components/Shared/IconComponent";
+import ButtonComponent from "../../components/Shared/ButtonComponent";
+import PoweredBY from "../../components/Shared/PoweredBy";
 
 export default class Account extends Component {
   constructor(props) {
@@ -13,61 +19,61 @@ export default class Account extends Component {
     this.state = {
       accountList: [
         {
-          name: 'Alfred Information',
+          name: "Alfred Information",
           lists: [
             {
-              name: 'Points Activity',
-              route: '',
+              name: "Points Activity",
+              route: "",
             },
             {
-              name: 'Personal Information',
-              route: 'PersonalInformation',
+              name: "Personal Information",
+              route: "PersonalInformation",
             },
             {
-              name: 'Member Benefits',
-              route: 'MemberBenefits',
+              name: "Member Benefits",
+              route: "MemberBenefits",
             },
             {
-              name: 'Alfred Card',
-              route: '',
+              name: "Alfred Card",
+              route: "",
             },
           ],
         },
         {
-          name: 'Preferences',
+          name: "Preferences",
           lists: [
             {
-              name: 'Feedback',
-              route: 'Feedback',
+              name: "Feedback",
+              route: "Feedback",
             },
             {
-              name: 'Favourites',
-              route: 'Favorites',
+              name: "Favourites",
+              route: "Favorites",
             },
             {
-              name: 'Dream Hotel Benefits',
-              route: '',
+              name: "Dream Hotel Benefits",
+              route: "",
             },
             {
-              name: 'Room Preferences',
-              route: 'Preferences',
+              name: "Room Preferences",
+              route: "Preferences",
             },
             {
-              name: 'Security',
-              route: 'Security',
+              name: "Security",
+              route: "Security",
             },
           ],
         },
         {
-          name: 'About',
+          name: "About",
           lists: [
             {
-              name: 'Legal',
-              route: '',
+              name: "Legal",
+              route: "",
             },
             {
-              name: 'Version',
-              route: '',
+              name: "Version",
+              route: "",
             },
           ],
         },
@@ -76,12 +82,13 @@ export default class Account extends Component {
   }
 
   renderList = () => {
-    const {accountList} = this.state;
+    const { accountList } = this.state;
     return accountList.map((list, i) => (
-      <View style={{paddingBottom: 10}} key={i}>
+      <View style={{ paddingBottom: 10 }} key={i}>
         <TextComponent
-          style={{fontSize: 15, paddingBottom: 10}}
-          type={FontType.BOLD}>
+          style={{ fontSize: 15, paddingBottom: 10 }}
+          type={FontType.BOLD}
+        >
           {list.name}
         </TextComponent>
         <View
@@ -90,42 +97,52 @@ export default class Account extends Component {
             borderColor: Colors.accordionBorderColor,
             borderRadius: 5,
             paddingBottom: 10,
-          }}>
+          }}
+        >
           {list.lists.map((list, i) => (
-            <TouchableNativeFeedback
+            // <TouchableNativeFeedback
+            //   key={i}
+            //   onPress={() =>
+            //     list.route && this.props.navigation.navigate(list.route)
+            //   }>
+            <TouchableOpacity
               key={i}
               onPress={() =>
                 list.route && this.props.navigation.navigate(list.route)
-              }>
-              <View style={{flex: 1}} key={i}>
-                <View
-                  style={{
-                    paddingHorizontal: 15,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    // paddingHorizontal: 15,
-                    paddingVertical: 15,
-                    //   backgroundColor: 'green',
-                    flex: 1,
-                  }}>
-                  <TextComponent style={{fontSize: 13}}>
-                    {list.name}
-                  </TextComponent>
-                  <IconComponent type={IconType.AntDesign} name="right" />
-                </View>
-                <View
-                  style={{
-                    position: 'absolute',
-                    height: 1,
-                    backgroundColor: Colors.accordionBorderColor,
-                    alignSelf: 'center',
-                    width: '96%',
-                    left: '2%',
-                    bottom: 0,
-                  }}></View>
+              }
+              style={{ flex: 1 }}
+              key={i}
+            >
+              <View
+                style={{
+                  paddingHorizontal: 15,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  // paddingHorizontal: 15,
+                  paddingVertical: 15,
+                  //   backgroundColor: 'green',
+                  flex: 1,
+                }}
+              >
+                <TextComponent style={{ fontSize: 13 }}>
+                  {list.name}
+                </TextComponent>
+                <IconComponent type={IconType.AntDesign} name="right" />
               </View>
-            </TouchableNativeFeedback>
+              <View
+                style={{
+                  position: "absolute",
+                  height: 1,
+                  backgroundColor: Colors.accordionBorderColor,
+                  alignSelf: "center",
+                  width: "96%",
+                  left: "2%",
+                  bottom: 0,
+                }}
+              ></View>
+            </TouchableOpacity>
+            // </TouchableNativeFeedback>
           ))}
         </View>
       </View>
@@ -138,23 +155,25 @@ export default class Account extends Component {
           paddingHorizontal: 15,
           paddingTop: 10,
           backgroundColor: Colors.white,
-        }}>
+        }}
+      >
         {this.renderList()}
         <ButtonComponent
-          onPress={() => this.props.navigation.navigate('GetStarted')}
+          onPress={() => this.props.navigation.navigate("GetStarted")}
           borderRadius={50}
           style={{
             backgroundColor: Colors.themeBlack,
-            width: '30%',
+            width: "30%",
             fontSize: 13,
             height: 40,
             color: Colors.white,
             elevation: 0,
-            alignSelf: 'center',
-          }}>
+            alignSelf: "center",
+          }}
+        >
           Log Out
         </ButtonComponent>
-        <View style={{paddingBottom: 15}}>
+        <View style={{ paddingBottom: 15 }}>
           <PoweredBY />
         </View>
       </ScrollView>
